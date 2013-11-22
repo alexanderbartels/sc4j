6842a95af0b887e4c122d7942cf4f161ebf6b8c5
Alexander Bartels <bartels.xander@googlemail.com>
2013-11-22 07:02:04 -0500

    initial test case commit

---
+++ src/test/java/com/bartels/sc4j/CommandLineArgumentsTest.java
@@ -0,0 +1,16 @@
+package com.bartels.sc4j;
+
+import com.bartels.sc4j.file.PropertyFile;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for using command line arguments to override properties
+ */
+public class CommandLineArgumentsTest extends TestCase {
+    
+    
+    
+}
