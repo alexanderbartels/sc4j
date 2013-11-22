b72871d5439a1e798bd2e298c50b82bb674c65dc
Alexander Bartels <bartels.xander@googlemail.com>
2013-11-22 07:32:44 -0500

    implemented test case for command line arguments

--- src/test/java/com/bartels/sc4j/CommandLineArgumentsTest.java
+++ src/test/java/com/bartels/sc4j/CommandLineArgumentsTest.java
@@ -12,6 +12,7 @@ import junit.framework.TestSuite;
 public class CommandLineArgumentsTest extends TestCase {
     
     // configuration interface to test
+    @PropertyFile("sc4j.properties")
     public interface ServerConfiguration {
         
         @DefaultValue("80")
@@ -21,6 +22,41 @@ public class CommandLineArgumentsTest extends TestCase {
         String serverHost();
     }
     
-    
-    
+
+	public CommandLineArgumentsTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(CommandLineArgumentsTest.class);
+	}
+	
+	public void testCompleteOverride() {
+	    String[] cmdArgs = new String[]{
+	        "--server.port", "5432",
+	        "--server.host", "localhost"
+	    };
+	    ServerConfiguration config = ConfigurationFactory.create(ServerConfiguration.class, cmdArgs);
+	    
+	    assertNotNull(config);
+	    
+	    assertEquals(5432, config.serverPort());
+	    assertEquals("localhost", config.serverHost());
+	}
+	
+	public void testDefaults() {
+	    String[] cmdArgs = new String[]{
+	        "--some.prop", "foo",
+	        "--some.other.prop", "bar"
+	    };
+	    ServerConfiguration config = ConfigurationFactory.create(ServerConfiguration.class, cmdArgs);
+	    
+	    assertNotNull(config);
+	    
+	    assertEquals(80, config.serverPort());
+	    assertEquals("127.0.0.1", config.serverHost());
+	}
 }
